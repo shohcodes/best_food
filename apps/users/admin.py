7756154f22a036d41276a168e8f92247197fbb20
shohcodes <zokirislom01@gmail.com>
2023-08-24 10:43:48 +0500

    Users admin panel updated

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -2,5 +2,64 @@ from django.contrib import admin
 
 from apps.users.models import User, TelegramUser
 
-admin.site.register(User)
-admin.site.register(TelegramUser)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['firstname', 'lastname', 'role']
+    list_display_links = ['firstname', 'lastname']
+    search_fields = ['firstname', 'lastname']
+    list_filter = ['role']
+    fieldsets = [
+        (
+            'Personal Information',
+            {
+                'fields': ['firstname', 'lastname']
+            }
+        ),
+        (
+            'Role',
+            {
+                'fields': ['role']
+            }
+        )
+    ]
+
+
+@admin.action(description='Block selected')
+def block_selected(modeladmin, request, queryset):
+    queryset.update(is_blocked=True)
+
+
+@admin.action(description='Unblock selected')
+def unblock_selected(modeladmin, request, queyset):
+    queyset.update(is_blocked=False)
+
+
+class TelegramUserAdmin(admin.ModelAdmin):
+    actions = [block_selected, unblock_selected]
+    list_display = ['fullname', 'role', 'chat_id', 'is_blocked']
+    search_fields = ['fullname', 'chat_id']
+    list_filter = ['role', 'is_blocked']
+    fieldsets = [
+        (
+            'Personal Information',
+            {
+                'fields': ['fullname', 'chat_id', 'role']
+            }
+        ),
+        (
+            'Status',
+            {
+                'fields': ['is_blocked']
+            }
+        ),
+        (
+            'Contact',
+            {
+                'fields': ['phone_number']
+            }
+        )
+    ]
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(TelegramUser, TelegramUserAdmin)
