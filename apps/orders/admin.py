36684d4cb1d5ec5623201a3e15886f53bdb9b00d
shohcodes <zokirislom01@gmail.com>
2023-08-24 20:45:22 +0500

    orders admin panel updated

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,5 +1,40 @@
 from django.contrib import admin
 
-from apps.orders.models import Order
+from apps.orders.models import Order, OrderFood
 
-admin.site.register(Order)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'telegram_user', 'status']
+    list_display_links = ['id', 'telegram_user']
+    search_fields = ['telegram_user', 'delivered_by']
+    list_filter = ['status']
+    fieldsets = [
+        (
+            'Main Info',
+            {
+                'fields': ['total_price', 'telegram_user', 'status']
+            }
+        ),
+        (
+            'Extra Info',
+            {
+                'fields': ['description', 'delivered_by', 'delivered_at']
+            }
+        ),
+        (
+            'Locations',
+            {
+                'fields': ['lat', 'lon']
+            }
+        )
+    ]
+
+
+class OrderFoodAdmin(admin.ModelAdmin):
+    list_display = ['order', 'food', 'amount', 'price']
+    list_display_links = ['order', 'food']
+    search_fields = ['food']
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderFood)
