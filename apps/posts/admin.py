94850fa8751351752efc0db459ae592d7070ff41
shohcodes <zokirislom01@gmail.com>
2023-08-24 20:58:32 +0500

    posts admin panel updated

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -2,4 +2,23 @@ from django.contrib import admin
 
 from apps.posts.models import Post
 
-admin.site.register(Post)
+
+@admin.action(description='Make post available')
+def make_available(modeladmin, request, queryset):
+    queryset.update(status='available')
+
+
+@admin.action(description='Make post unavailable')
+def make_unavailable(modeladmin, request, queryset):
+    queryset.update(status='unavailable')
+
+
+class PostAdmin(admin.ModelAdmin):
+    actions = [make_available, make_unavailable]
+    list_display = ['title', 'content', 'status']
+    list_display_links = ['title', 'content']
+    list_filter = ['status']
+    search_fields = ['title']
+
+
+admin.site.register(Post, PostAdmin)
