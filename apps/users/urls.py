abf9b4de3473d7fafd8aa7ac03fa16b47da08014
shohcodes <zokirislom01@gmail.com>
2023-08-29 00:43:02 +0500

    urls updated

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from apps.users.views import UserViewSet
+from apps.users.views import UserViewSet, TelegramUserViewSet
 
 router = DefaultRouter()
 router.register('users', UserViewSet, basename='users')
+router.register('tg-users', TelegramUserViewSet, basename='tg-users')
 
 urlpatterns = router.urls
