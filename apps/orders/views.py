5e847ecea9ba1873b3d1fb7a6b4dab5ab8a22cc7
shohcodes <zokirislom01@gmail.com>
2023-08-29 17:46:37 +0500

    views created

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from apps.orders.filters import OrderStatusFilter
+from apps.orders.models import Order
+from apps.orders.serializers import OrderDetailSerializer, OrderListSerializer
+
+
+class OrderViewSet(ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderDetailSerializer
+    filter_backends = [OrderStatusFilter]
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return OrderListSerializer
+        return self.serializer_class
+
