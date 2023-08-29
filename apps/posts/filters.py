673fd19a550db65ec8a0d569c4a68e8110b59e68
shohcodes <zokirislom01@gmail.com>
2023-08-29 16:21:59 +0500

    filters created for posts

---
+++ apps/posts/filters.py
@@ -0,0 +1,12 @@
+from rest_framework.filters import BaseFilterBackend
+
+
+class PostStatusFilter(BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        status = request.query_params.get('status')
+        if status:
+            if status == '1':
+                queryset = queryset.filter(status='available')
+            elif status == '0':
+                queryset = queryset.filter(status='unavailable')
+        return queryset
