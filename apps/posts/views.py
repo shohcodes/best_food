ccc064a2fae43e959e75ac6c27c8d5f32b9e8127
shohcodes <zokirislom01@gmail.com>
2023-08-29 16:19:59 +0500

    created new views

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from apps.posts.filters import PostStatusFilter
+from apps.posts.models import Post
+from apps.posts.serializers import PostDetailSerializer, PostListSerializer
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    filter_backends = [PostStatusFilter]
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PostListSerializer
+        return self.serializer_class
