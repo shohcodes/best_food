f9aa5c87701ec494154267ec1cca0e67d8650841
shohcodes <zokirislom01@gmail.com>
2023-09-01 22:12:08 +0500

    views updated

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,11 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 
-from apps.users.filters import UserRoleFilter, TelegramUserRoleFilter, TelegramUserIsBlockedFilter
+from apps.users.filters import UserRoleFilter, TelegramUserIsBlockedFilter, TelegramUserIsVerifiedFilter
 from apps.users.models import User, TelegramUser
 from apps.users.serializers import UserDetailSerializer, UserListSerializer, TelegramUserDetailSerializer, \
     TelegramUserListSerializer
 
 
+# USER VIEWS
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserDetailSerializer
@@ -17,10 +18,11 @@ class UserViewSet(ModelViewSet):
         return self.serializer_class
 
 
+# TELEGRAM_USER_VIEWS
 class TelegramUserViewSet(ModelViewSet):
     queryset = TelegramUser.objects.all()
     serializer_class = TelegramUserDetailSerializer
-    filter_backends = [TelegramUserIsBlockedFilter, TelegramUserRoleFilter]
+    filter_backends = [TelegramUserIsBlockedFilter, TelegramUserIsVerifiedFilter]
 
     def get_serializer_class(self):
         if self.action == 'list' or self.action == 'retrieve':
