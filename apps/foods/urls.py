282ccd1c843877c341651ad2aa5c0683f1e2065b
shohcodes <zokirislom01@gmail.com>
2023-09-01 21:52:39 +0500

    added new urls

--- apps/foods/urls.py
+++ apps/foods/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from apps.foods.views import FoodViewSet
+from apps.foods.views import FoodViewSet, CategoryViewSet
 
 router = DefaultRouter()
 router.register('foods', FoodViewSet, 'foods')
+router.register('categories', CategoryViewSet, 'categories')
 
 urlpatterns = router.urls
