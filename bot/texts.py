74f63f3aa7e607b04a04879e5503719e19393dd6
shohcodes <zokirislom01@gmail.com>
2023-09-02 14:45:16 +0500

    new texts for bot

---
+++ bot/texts.py
@@ -0,0 +1,19 @@
+from bot import api
+
+ENTER_NAME = 'To`liq ismingizni kiriting'
+ENTER_NUMBER = 'Telefon raqamingizni kiriting'
+MENU = '🍽️ Menyu'
+MY_ORDERS = '🛍️ Buyurtmalarim'
+ABOUT_US = 'ℹ️ Biz haqimizda'
+BASKET = '🛒 Savat'
+CHOOSE_MENU = 'Quyidagilardan birini tanlang 👇'
+CHOOSE_CATEGORY = 'Kategoriyalardan birini tanlang 👇'
+FOODS = '🥮 Ovqatlar'
+NO_FOODS = 'Hozircha hech qanday ovqat mavjud emas'
+ABOUT_US_TEXT = ('Biz O`zbekiston bozorida 1 yildan beri faoliyat yuritamiz va bugungi kunda butun mamlakat '
+                 'bo`ylab 2 dan ortiq filiallarimiz mavjud. Mazali va to`yimli taomlar, qulay narxlar, tez yetkazib'
+                 'berish xizmatidan mamnun mijozlar yana va yana bizni tanlamoqda.\n\n'
+                 'Qaynoqqina va mazali lavashlarimiz, shaurmayu donerlarimiz, gamburger va pitsalarimizdan '
+                 'albatta tatib ko`rishizni tavsiya qilamiz va buyurtmangizga tovuq go`shtidan yangiliklarimizni'
+                 ' qo`shishni unutmang!\n\n Yetkazib berish xizmati: 127.0.0.1:8000'
+                 )
