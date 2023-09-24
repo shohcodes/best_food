072fd4b70f429eccb6a982ec70509d01bbf02e81
shohcodes <zokirislom01@gmail.com>
2023-09-24 21:06:42 +0500

    basket urls configured

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from apps.orders.views import OrderViewSet
+from apps.orders.views import OrderViewSet, BasketViewSet
 
 router = DefaultRouter()
 router.register('orders', OrderViewSet, 'orders')
+router.register('basket', BasketViewSet, 'basket')
 
 urlpatterns = router.urls
