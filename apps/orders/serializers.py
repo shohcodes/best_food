38b6084798cf913f11d3dd334feb5b0dce5289c5
shohcodes <zokirislom01@gmail.com>
2023-09-24 21:06:18 +0500

    basket serializers created

--- apps/orders/serializers.py
+++ apps/orders/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from apps.orders.models import Order, OrderFood
+from apps.orders.models import Order, OrderFood, Basket
 
 
 class OrderListSerializer(serializers.ModelSerializer):
@@ -25,3 +25,25 @@ class OrderFoodDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderFood
         fields = '__all__'
+
+
+class BasketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Basket
+        fields = '__all__'
+
+    def create(self, validated_data):
+        telegram_user = validated_data['telegram_user']
+        food = validated_data['food']
+        amount = validated_data['amount']
+
+        existing_basket = Basket.objects.filter(telegram_user=telegram_user, food=food).first()
+
+        if existing_basket:
+            existing_basket.amount += amount
+            existing_basket.save()
+            return existing_basket
+        else:
+            return Basket.objects.create(**validated_data)
+
+        
